7fa5b69365f0c4c9b3ffd5dab623d170e79c3f37
Daniel Dada <gloriachurch69x@gmail.com>
2018-02-26 14:39:16 +0300

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+if [ "$@" ]; then
+	commit_message="$@"
+else
+	commit_message="content update"
+fi
+mkdocs build &&
+git add * &&
+git commit -m "$commit_message" &&
+git push origin master
